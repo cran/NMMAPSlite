9c1ad75b751aeb5e1f3d2cc508820622adcbd722
Roger D. Peng <rpeng@jhsph.edu>
2007-06-18 00:00:00 +0000

    version 0.3-1

--- R/zzz.R
+++ R/zzz.R
@@ -1,23 +1,23 @@
 .onLoad <- function(lib, pkg) {
-    pkgs <- c("stashR")
+        pkgs <- c("stashR")
 
-    for(pkg in pkgs) {
-        if(!require(pkg, quietly = TRUE, character.only = TRUE))
-            stop(gettextf("'%s' package required", pkg))
-    }
-    stashROption("quietDownload", TRUE)
+        for(pkg in pkgs) {
+                if(!require(pkg, quietly = TRUE, character.only = TRUE))
+                        stop(gettextf("'%s' package required", pkg))
+        }
+        stashROption("quietDownload", TRUE)
 }
 
 .onAttach <- function(lib, pkg) {
-    dcf <- read.dcf(file.path(lib, pkg, "DESCRIPTION"))
-    msg <- gettextf("%s (%s %s)", dcf[, "Title"],
-                    as.character(dcf[, "Version"]), dcf[, "Date"])
-    message(paste(strwrap(msg), collapse = "\n"))
-    message(paste(strwrap(gettext("Initialize database using 'initDB'")),
-                  collapse = "\n"))
+        dcf <- read.dcf(file.path(lib, pkg, "DESCRIPTION"))
+        msg <- gettextf("%s (%s %s)", dcf[, "Title"],
+                        as.character(dcf[, "Version"]), dcf[, "Date"])
+        message(paste(strwrap(msg), collapse = "\n"))
+        message(paste(strwrap(gettext("Initialize database using 'initDB'")),
+                      collapse = "\n"))
 }
 
 .dbEnv <- new.env()
 
 
-               
+
